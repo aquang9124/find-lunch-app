b69e2664ddd63b836307e59bb5ff47c59bdd0260
Alex Quang <alexquang1991@gmail.com>
2016-06-29 10:02:40 -0700

    finished about us section and header

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,8 +5,15 @@ export default Ember.Route.extend({
 	actions: {
 		jumpToAnchor: function(element) {
 			$('html, body').animate({
-				scrollTop: $('#' + element).offset().top
+				scrollTop: $('#' + element).offset().top - 50
 			}, 600);
+		},
+		toggleText: function() {
+			$('.header-text').click(function() {
+				$(this).text(function(_, text) {
+					return text === "Find Lunch" ? "Quick and Easy" : "Find Lunch";
+				});
+			});
 		}
 	}
 });
